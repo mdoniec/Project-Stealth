7f946e7a95549298eec3eb4b32e4e95cf423303e
karolmatyasik <karolmatyasik@wp.pl>
2015-01-04 23:42:04 +0100

    Scarry sounds + scripting added 0.1 , guitars + telephone booth

---
+++ RayAV/Assets/ScarrySounds.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScarrySounds : MonoBehaviour {
+
+	public AudioSource source;
+	public AudioClip fallsound;
+	public GameObject player;
+	public GameObject fallplace;
+	bool fall = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ((player.transform.position-fallplace.transform.position).magnitude<2f && fall==false) {
+			source.PlayOneShot(fallsound,1f);
+			fall=true;
+	}
+}
+}
